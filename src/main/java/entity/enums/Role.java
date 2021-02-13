ccff48f94b58ceca909dec7f42e126fbc21bee93
FMpkPLtG <yuraukr096@gmail.com>
2021-02-13 16:28:50 +0200

    initial commit

---
+++ src/main/java/entity/enums/Role.java
@@ -0,0 +1,9 @@
+package entity.enums;
+
+/**
+ * @author yuriismac on 2/13/21.
+ * @project travel_agency
+ */
+public enum  Role {
+    USER, MANAGER;
+}
---
+++ src/main/java/entity/enums/RoomType.java
@@ -0,0 +1,10 @@
+package entity.enums;
+
+/**
+ * @author yuriismac on 2/13/21.
+ * @project travel_agency
+ */
+public enum RoomType {
+
+    SINGLE, DOUBLE, TRIPLE;
+}
